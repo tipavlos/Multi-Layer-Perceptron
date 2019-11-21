7275d0166627c6fa919353bf17fc0aada99b1728
tipavlos <pavlos27t@gmail.com>
2019-11-21 18:13:16 +0200

    Example
    training and testing with mnist

---
+++ Example.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <iostream>
+#include "NeuralNetwork.h"
+#include "mnist\mnist_reader_less.hpp"
+#include "mnist\mnist_utils.hpp"
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	auto dataset = mnist::read_dataset<double, uint8_t>();
+	normalize_dataset(dataset);
+	vector<vector<bool>> y;
+	vector<bool> zeros(10, false);
+	for (auto v : dataset.training_labels) {
+		y.push_back(zeros);
+		y.back()[v] = 1;
+	}
+	int hd[]{ 128 };
+	MultiLayerPerceptron *nn = new MultiLayerPerceptron(hd, 1, 10, dataset.training_images[0].size(), 0.01, "tanh", 100, 50);
+	nn->fit<double, bool>(dataset.training_images, y);
+
+	float acc = 0;
+	for (int i = 0; i < dataset.test_images.size(); i++) {
+		acc += (nn->predict_best_class(dataset.test_images[i]) == dataset.test_labels[i]);
+	}
+	cout << "Accuracy : " << acc / dataset.test_images.size() << endl;
+}
